d7335bb25d50b076c99cf9640eabe8837c1d9ab4
Beniamino Green <ben@greendalba.com>
2023-02-15 10:39:48 -0500

    updated documentation

--- R/jaccard_similarity.R
+++ R/jaccard_similarity.R
@@ -1,5 +1,12 @@
 #' Calculate jaccard_similarity of two character vectors
-#`
+#'
+#' @param a the first character vector
+#' @param b the first character vector
+#'
+#' @param ngram_width the length of the shingles / ngrams used in the
+#' similarity calculation
+#'
+#'
 #' @export
 jaccard_similarity <- function(a, b, ngram_width=2) {
     stopifnot(length(a) == length(b))
--- R/lsh_properties.R
+++ R/lsh_properties.R
@@ -30,6 +30,9 @@ lsh_curve <- function(n_bands, band_width) {
 #'
 #' @param band_width: The number of hashes in each band
 #'
+#' @return a decimal number giving the proability that the two items will be
+#' returned as a candidate pair from the minihash algotithm.
+#'
 #' @export
 lsh_probability <- function(similarity, n_bands, band_width){
     1-(1-similarity^band_width)^n_bands
