8d87b14c11d9a83bea59cd52bbb49db7d3bf1da6
Beniamino Green <ben@greendalba.com>
2023-03-08 22:38:31 -0500

    kdtree

--- tests/testthat/test-test_kd_join_core.R
+++ tests/testthat/test-test_kd_join_core.R
@@ -1,5 +1,5 @@
 test_that("kd_join_core works on toy datasets", {
-    for (i in 1:20) {
+    for (i in 1:30) {
     capture_messages({
         n <- 2000
         X_1 <- matrix(rnorm(n*2), nrow=n)
@@ -7,7 +7,14 @@ test_that("kd_join_core works on toy datasets", {
         X_1 <- as.data.frame(X_1)
         X_2 <- as.data.frame(X_2)
 
-        expect_equal(nrow(kd_join_core(X_1, X_2, radius=.00005)), 2000)
+        X_1$id_1 <- 1:n
+        X_2$id_2 <- 1:n
+
+        join_out <- kd_join_core(X_1, X_2, radius=.00005)
+
+        expect_true(all(join_out$id_1 == join_out$id_2))
+
+        expect_equal(nrow(join_out), 2000)
     })
     }
 })
