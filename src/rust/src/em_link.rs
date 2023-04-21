6ac8ead039d23c6c7e7a617acbb9f6b18f25d35e
Beniamino Green <ben@greendalba.com>
2023-04-21 15:05:42 -0400

    stashing EM progress for now

---
+++ src/rust/src/em_link.rs
@@ -0,0 +1,140 @@
+use itertools::Itertools;
+use std::collections::hash_map::DefaultHasher;
+use std::collections::HashMap;
+use std::hash::{Hash, Hasher};
+
+use extendr_api::prelude::*;
+
+struct AgreeBundle {
+    pattern: Array1<usize>,
+    ids: Vec<usize>,
+    n: f64,
+    prob_match: f64,
+}
+
+impl AgreeBundle {
+    fn new(pattern: ArrayView1<usize>, id: usize, prob : f64) -> Self {
+        Self {
+            pattern: pattern.to_owned(),
+            ids: vec![id],
+            n: 1.0,
+            prob_match: prob,
+        }
+    }
+
+    fn add(&mut self, id: usize) {
+        self.ids.push(id);
+        self.n += 1.0
+    }
+}
+
+pub struct EMLinker {
+    bundles: Vec<AgreeBundle>,
+    n: f64,
+    match_params: Vec<Vec<f64>>,
+    not_match_params: Vec<Vec<f64>>,
+    lambda: f64,
+}
+
+impl EMLinker {
+    pub fn new(x_mat: ArrayView2<usize>, guesses : &[f64]) -> Self {
+        let mut agree_collection: HashMap<u64, AgreeBundle> = HashMap::new();
+        let mut match_params = Vec::new();
+        let mut not_match_params = Vec::new();
+
+        for col in x_mat.axis_iter(Axis(1)) {
+            let unique = col.iter().unique().collect::<Vec<&usize>>().len();
+
+            match_params.push(vec![0.0; unique]);
+            not_match_params.push(vec![0.0; unique]);
+        }
+
+        for (i, row) in x_mat.axis_iter(Axis(0)).enumerate() {
+            let mut hasher = DefaultHasher::new();
+            row.hash(&mut hasher);
+            let key = hasher.finish();
+
+            match agree_collection.get_mut(&key) {
+                Some(bundle) => bundle.add(i),
+                None => {
+                    agree_collection.insert(key, AgreeBundle::new(row, i, guesses[i]));
+                }
+            }
+        }
+
+        Self {
+            bundles: agree_collection.into_values().collect(),
+            n: x_mat.nrows() as f64,
+            match_params,
+            not_match_params,
+            lambda: 0.5,
+        }
+    }
+
+    fn m_step(&mut self) {
+
+        // update lambda
+        self.lambda = self
+            .bundles
+            .iter()
+            .map(|bundle| bundle.prob_match * bundle.n)
+            .sum::<f64>()
+            / self.n;
+
+        // set paramaters to zero
+        for variable in &mut self.match_params {
+            for paramater in variable.iter_mut(){
+                *paramater = 0.0;
+            }
+        }
+
+        for variable in self.not_match_params.iter_mut() {
+            for paramater in variable.iter_mut(){
+                *paramater = 0.0;
+            }
+        }
+
+        // update match and not_match params
+        for bundle in &self.bundles {
+            for (i, agree_level) in bundle.pattern.iter().enumerate() {
+                self.match_params[i][*agree_level] += bundle.n*bundle.prob_match/(self.n*self.lambda);
+                self.not_match_params[i][*agree_level] += bundle.n*(1.0 - bundle.prob_match)/(self.n * (1.0 - self.lambda));
+            }
+        }
+
+    }
+
+    fn e_step(&mut self) {
+        for bundle in &mut self.bundles {
+            let mut match_likelihood = 1.0;
+            let mut not_match_likelihood = 1.0;
+
+            for (i, agree_level) in bundle.pattern.iter().enumerate() {
+                match_likelihood *= self.match_params[i][*agree_level];
+                not_match_likelihood *= self.not_match_params[i][*agree_level];
+            }
+
+            bundle.prob_match = self.lambda * match_likelihood /
+                (self.lambda * match_likelihood + (1.0-self.lambda) * not_match_likelihood);
+        }
+
+    }
+
+    pub fn link(&mut self) -> Vec<f64>{
+        self.m_step();
+        for _ in 0..10 {
+            self.e_step();
+            self.m_step();
+        }
+
+        let mut out_vec = vec![0.0; self.n as usize];
+
+        for bundle in self.bundles.iter() {
+            for i in bundle.ids.iter() {
+                out_vec[*i] = bundle.prob_match;
+            }
+        }
+
+        out_vec
+    }
+}
--- src/rust/src/lib.rs
+++ src/rust/src/lib.rs
@@ -1,11 +1,6 @@
 use extendr_api::prelude::parallel::prelude::ParallelIterator;
 use extendr_api::prelude::*;
 
-use itertools::Itertools;
-use std::collections::hash_map::DefaultHasher;
-use std::collections::HashMap;
-use std::hash::{Hash, Hasher};
-
 use rayon::prelude::*;
 
 use kdtree::distance::squared_euclidean;
@@ -14,148 +9,25 @@ use kdtree::KdTree;
 pub mod shingleset;
 use crate::shingleset::ShingleSet;
 
+pub mod em_link;
+use crate::em_link::EMLinker;
+
 pub mod minihasher;
 
 pub mod lshjoiner;
 use crate::lshjoiner::LSHjoiner;
 
-#[derive(Debug)]
-struct AgreeBundle {
-    pattern: Array1<usize>,
-    ids: Vec<usize>,
-    n: f64,
-    prob_match: f64,
-}
-
-impl AgreeBundle {
-    fn new(pattern: ArrayView1<usize>, id: usize) -> Self {
-        Self {
-            pattern: pattern.to_owned(),
-            ids: vec![id],
-            n: 1.0,
-            prob_match: 0.5,
-        }
-    }
-
-    fn add(&mut self, id: usize) {
-        self.ids.push(id);
-        self.n += 1.0
-    }
-}
-
-#[derive(Debug)]
-struct EMLinker {
-    bundles: Vec<AgreeBundle>,
-    n: f64,
-    match_params: Vec<Vec<f64>>,
-    not_match_params: Vec<Vec<f64>>,
-    lambda: f64,
-}
-
-impl EMLinker {
-    fn new(x_mat: ArrayView2<usize>) -> Self {
-        let mut agree_collection: HashMap<u64, AgreeBundle> = HashMap::new();
-        let mut match_params = Vec::new();
-        let mut not_match_params = Vec::new();
-
-        for col in x_mat.axis_iter(Axis(1)) {
-            let unique = col.iter().unique().collect::<Vec<&usize>>().len();
-
-            match_params.push(vec![0.0; unique]);
-            not_match_params.push(vec![0.0; unique]);
-        }
-
-        for (i, row) in x_mat.axis_iter(Axis(0)).enumerate() {
-            let mut hasher = DefaultHasher::new();
-            row.hash(&mut hasher);
-            let key = hasher.finish();
-
-            match agree_collection.get_mut(&key) {
-                Some(bundle) => bundle.add(i),
-                None => {
-                    agree_collection.insert(key, AgreeBundle::new(row, i));
-                }
-            }
-        }
-
-        Self {
-            bundles: agree_collection.into_values().collect(),
-            n: x_mat.nrows() as f64,
-            match_params,
-            not_match_params,
-            lambda: 0.0,
-        }
-    }
-
-    fn m_step(&mut self) {
-
-        // update lambda
-        self.lambda = self
-            .bundles
-            .iter()
-            .map(|bundle| bundle.prob_match * bundle.n)
-            .sum::<f64>()
-            / self.n;
-
-        // set paramaters to zero
-        for variable in &mut self.match_params {
-            for paramater in variable.iter_mut(){
-                *paramater = 0.0;
-            }
-        }
-
-        for variable in self.not_match_params.iter_mut() {
-            for paramater in variable.iter_mut(){
-                *paramater = 0.0;
-            }
-        }
-
-        // update match and not_match params
-        for bundle in &self.bundles {
-            for (i, agree_level) in bundle.pattern.iter().enumerate() {
-                self.match_params[i][*agree_level] += bundle.n*bundle.prob_match;
-                self.not_match_params[i][*agree_level] += bundle.n*(1.0 - bundle.prob_match);
-            }
-        }
-
-    }
-
-    fn e_step(&mut self) {
-        // classify each unit
-        for bundle in &mut self.bundles {
-            let mut match_likelihood = 1.0;
-            let mut not_match_likelihood = 1.0;
-
-            for (i, agree_level) in bundle.pattern.iter().enumerate() {
-                match_likelihood *= self.match_params[i][*agree_level];
-                not_match_likelihood *= self.not_match_params[i][*agree_level];
-            }
-
-            bundle.prob_match = self.lambda * match_likelihood / (match_likelihood + not_match_likelihood);
-        }
-
-    }
-
-    fn link(&mut self) {
-        for i in 0..1000 {
-            println!("{i}");
-            self.e_step();
-            self.m_step();
-        }
-
-    }
-}
-
 #[extendr]
-fn agreement_linker(x_robj: Robj) {
+fn em_link(x_robj: Robj, probs : &[f64]) -> Vec<f64>{
     let x_mat = <ArrayView2<i32>>::from_robj(&x_robj)
         .unwrap()
         .to_owned()
         .map(|x| *x as usize);
 
-    let mut linker = EMLinker::new(x_mat.view());
-    linker.link();
-    println!("constructed!");
+    let mut linker = EMLinker::new(x_mat.view(), probs);
+    let out = linker.link();
+    // println!("constructed!");
+    out
 }
 
 #[extendr]
@@ -286,5 +158,5 @@ extendr_module! {
     fn rust_salted_lsh_join;
     fn rust_kd_join;
     fn rust_jaccard_similarity;
-    fn agreement_linker;
+    fn em_link;
 }
