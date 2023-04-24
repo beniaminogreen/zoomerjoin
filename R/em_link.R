3f22bf1ca9ce4538f54304f28e2148b6cb9bcf5e
Beniamino Green <ben@greendalba.com>
2023-04-23 23:17:13 -0400

    added more em_link documentation

--- R/em_link.R
+++ R/em_link.R
@@ -1,10 +1,20 @@
 #' Fit a Probabilistic Matching Model using Naive Bayes + E.M.
 #'
-#' @param X a matrix of similarities
-#' @param g a vector of initial guesses
-#' @param tol tolerance in the sense of the infinity norm. i.e.
+#' @param X an integer matrix of similarities. Must go from 0 (the most
+#' disagreement) to the maximum without any "gaps" or unused levels. As an
+#' example, a column with values 0,1,2,3 is a valid column, but 0,1,2,4 is not
+#' as three is omitted
 #'
-#' @param max_iter iterations after which the algorithm will error out if it has not converged
+#' @param g a vector of initial guesses that are iteratively improved using the
+#' EM algorithm (my personal approach is to guess at logistic regression
+#' coefficients and use them to create the intitial probability guesses)
+#'
+#' @param tol tolerance in the sense of the infinity norm. i.e. how close the
+#' parameters have to be between iterations before the EM algorithm terminates.
+#'
+#' @param max_iter iterations after which the algorithm will error out if it
+#' has not converged.
+#'
 #' @return a vector of probabilities representing the posterior probability
 #' each record pair is a match.
 #'
@@ -25,6 +35,10 @@
 #'         as.integer(ifelse(d, runif(n)<.1, runif(n)<.9)),
 #'         as.integer(ifelse(d, runif(n)<.8, runif(n)<.01))
 #'         )
+#'
+#' # inital guess at class assignments based on
+#' # a hypothetical logistic regression. Should be based on domain knowledge.
+#'
 #'x_sum <- rowSums(X)
 #'g <- inv_logit((x_sum - mean(x_sum))/sd(x_sum))
 #'
