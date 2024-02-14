a2b802fc1f529b09f8ae310ff9c52f1270dd8f00
Etienne Bacher <52219252+etiennebacher@users.noreply.github.com>
2024-02-14 17:57:03 +0100

    clean docs

--- R/hamming_logical_joins.R
+++ R/hamming_logical_joins.R
@@ -1,43 +1,31 @@
 #' Fuzzy joins for Hamming distance using Locality Sensitive Hashing
 #'
 #' Find similar rows between two tables using the hamming distance. The hamming
-#' distance is equal to the number characters two strings differ by, or is
-#' equal to infinity if two strings are of different lengths
+#' distance is equal to the number characters two strings differ by, or is equal
+#' to infinity if two strings are of different lengths
 #'
+#' @inheritParams jaccard_left_join
 #'
-#' @param a the first dataframe you wish to join.
+#' @param n_bands The number of bands used in the locality sensitive hashing
+#'   algorithm (default is 100). Use this in conjunction with the
+#'   \code{band_width} to determine the performance of the hashing. Generally
+#'   speaking, a higher number of bands leads to greater recall at the cost of
+#'   higher runtime.
 #'
-#' @param b the second dataframe you wish to join.
+#' @param band_width The length of each band used in the minihashing algorithm
+#'   (default is 8). Use this in conjunction with the \code{n_bands} to determine
+#'   the performance of the hashing. Generally speaking a wider number of bands
+#'   decreases the number of false positives, decreasing runtime at the cost of
+#'   lower sensitivity (true matches are less likely to be found).
 #'
-#' @param by a named vector indicating which columns to join on. Format should
-#' be the same as dplyr: \code{by = c("column_name_in_df_a" = "column_name_in_df_b")}, but
-#' two columns must be specified in each dataset (x column and y column). Specification
-#' made with `dplyr::join_by()` are also accepted.
+#' @param similarity_column An optional character vector. If provided, the data
+#'   frame will contain a column with this name giving the Hamming distance
+#'   between the two fields. Extra column will not be present if anti-joining.
 #'
-#' @param n_bands the number of bands used in the locality sensitive hashing
-#' algorithm (default is 100). Use this in conjunction with the
-#' \code{band_width} to determine the performance of the hashing. Generally
-#' speaking, a higher number of bands leads to greater recall at the cost of
-#' higher runtime.
-#'
-#' @param band_width the length of each band used in the minihashing algorithm
-#' (default is 8) Use this in conjunction with the \code{n_bands} to determine
-#' the performance of the hashing. Generally speaking a wider number of bands
-#' decreases the number of false positives, decreasing runtime at the cost of
-#' lower sensitivity (true matches are less likely to be found).
-#'
-#' @param clean should the strings that you fuzzy join on be cleaned (coerced
-#' to lower-case, stripped of punctuation and spaces)? Default is FALSE
-#'
-#' @param progress set to `TRUE` to print progress
-#'
-#' @param similarity_column an optional character vector. If provided, the data
-#' frame will contain a column with this name giving the jaccard similarity
-#' between the two fields. Extra column will not be present if anti-joining.
-#'
-#' @return a tibble fuzzily-joined on the basis of the variables in `by.` Tries
-#' to adhere to the same standards as the dplyr-joins, and uses the same
-#' logical joining patterns (i.e. inner-join joins and keeps only observations in both datasets).
+#' @return A tibble fuzzily-joined on the basis of the variables in `by.` Tries
+#'   to adhere to the same standards as the dplyr-joins, and uses the same
+#'   logical joining patterns (i.e. inner-join joins and keeps only observations
+#'   in both datasets).
 #'
 #' @rdname hamming-joins
 #' @export
@@ -75,7 +63,7 @@
 hamming_inner_join <- function(a, b,
                                by = NULL,
                                n_bands = 100,
-                               band_width = 100,
+                               band_width = 8,
                                threshold = 2,
                                progress = FALSE,
                                clean = FALSE,
